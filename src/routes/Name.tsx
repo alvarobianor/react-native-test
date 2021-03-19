1cd49bc204e04cd4df2c31621c266465a74eb060
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2021-03-19 19:57:17 -0300

    push notification

--- src/routes/Name.tsx
+++ src/routes/Name.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, Button} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 import * as S from '../styles/style';
 import api from '../api';
 
 const Name = ({route}: any) => {
   const {name} = route.params;
 
+  const [token, setToken] = useState<string>('');
   const notification = async () => {
-    const resp = await api.post('/store', {name});
+    const resp = await api.post('/store', {name, token});
     console.log(resp);
   };
+
+  useEffect(() => {
+    messaging()
+      .getToken()
+      .then((token) => {
+        setToken(token);
+        console.log('tokeeen', token);
+      });
+  }, []);
 
   return (
     <S.Container>
